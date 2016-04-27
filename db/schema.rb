9de6dca4b3c2ace4211ad7fa319da63df436bf76
ottjw <ottjw@yahoo.com>
2016-04-27 09:01:43 +0000

    Got twitter images working!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412031748) do
+ActiveRecord::Schema.define(version: 20160427075911) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160412031748) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "twitter"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
