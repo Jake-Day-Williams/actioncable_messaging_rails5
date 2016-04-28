443eccc4126e7f9cdac2e916a4b02fbdd0f113c5
ottjw <ottjw@yahoo.com>
2016-04-28 20:04:48 +0000

    Added ability to share links in messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
     belongs_to :user
+
+    before_save do
+      self.content.gsub!(/((?:http|https):\/\/[^\s\n]*)/, "<a href='\\1'>\\1</a>")
+    end
+
     after_create_commit { BroadcastMessageJob.perform_later self }
 end
